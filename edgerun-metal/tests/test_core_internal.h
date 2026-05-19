2582810360410832cf89f2ed0fd131fdf04d4dc6
ken <ken@fw>
2026-05-20 04:00:43 +0700

    Add jurisdiction policy records

--- edgerun-metal/tests/test_core_internal.h
+++ edgerun-metal/tests/test_core_internal.h
@@ -23,6 +23,7 @@
 #include "er_identity.h"
 #include "er_iwlwifi.h"
 #include "er_hw_relay.h"
+#include "er_jurisdiction.h"
 #include "er_mt7922.h"
 #include "er_native_eth.h"
 #include "er_native_boot.h"
