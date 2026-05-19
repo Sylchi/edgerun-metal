e4dfef5230f53454090dafa9a53c9f850c51fc2b
ken <ken@fw>
2026-05-20 04:33:01 +0700

    Add Pi Zero 2W SDIO command plans

--- edgerun-metal/core/er_pi_zero2w.h
+++ edgerun-metal/core/er_pi_zero2w.h
@@ -39,6 +39,36 @@
 #define ER_PI_CLOCK_ID_EMMC 1u
 #define ER_PI_CLOCK_ID_UART 2u
 
+#define ER_PI_MMC_CMD_GO_IDLE_STATE 0u
+#define ER_PI_MMC_CMD_SEND_RELATIVE_ADDR 3u
+#define ER_PI_MMC_CMD_IO_SEND_OP_COND 5u
+#define ER_PI_MMC_CMD_SELECT_CARD 7u
+#define ER_PI_MMC_CMD_IO_RW_DIRECT 52u
+#define ER_PI_MMC_CMD_IO_RW_EXTENDED 53u
+
+#define ER_PI_MMC_RESPONSE_NONE 0u
+#define ER_PI_MMC_RESPONSE_R1 1u
+#define ER_PI_MMC_RESPONSE_R4 4u
+#define ER_PI_MMC_RESPONSE_R5 5u
+#define ER_PI_MMC_RESPONSE_R6 6u
+
+#define ER_PI_SDIO_FUNCTION_BACKPLANE 1u
+#define ER_PI_SDIO_FUNCTION_WLAN 2u
+
+#define ER_PI_SDIO_READ 0u
+#define ER_PI_SDIO_WRITE 1u
+#define ER_PI_SDIO_CMD52_READ ER_PI_SDIO_READ
+#define ER_PI_SDIO_CMD52_WRITE ER_PI_SDIO_WRITE
+#define ER_PI_SDIO_CMD52_NO_RAW 0u
+#define ER_PI_SDIO_CMD52_RAW 1u
+
+#define ER_PI_SDIO_CMD53_FIXED_ADDRESS 0u
+#define ER_PI_SDIO_CMD53_INCREMENTING_ADDRESS 1u
+#define ER_PI_SDIO_CMD53_BYTE_MODE 0u
+#define ER_PI_SDIO_CMD53_BLOCK_MODE 1u
+
+#define ER_PI_ZERO2W_SDIO_BRINGUP_COMMAND_CAPACITY 8u
+
 typedef struct {
   UINT8 mapped;
   INT64 peripheral_handle;
@@ -60,12 +90,42 @@ typedef struct {
   UINT32 end_tag;
 } ErPiMailboxTwoValueMessage;
 
+typedef struct {
+  UINT32 command_index;
+  UINT32 argument;
+  UINT32 response_kind;
+} ErPiMmcCommand;
+
+typedef struct {
+  UINT32 command_count;
+  ErPiMmcCommand commands[ER_PI_ZERO2W_SDIO_BRINGUP_COMMAND_CAPACITY];
+} ErPiZero2wSdioBringupPlan;
+
 UINT8 er_pi_zero2w_mmio_map(ErPiZero2wMmio* out_mmio);
 UINT64 er_pi_zero2w_peripheral_phys(UINT64 offset);
 UINT8 er_pi_mailbox_two_value_request(UINT32 tag_id,
                                       UINT32 value0,
                                       UINT32 value1,
                                       ErPiMailboxTwoValueMessage* out_message);
+UINT32 er_pi_sdio_cmd52_argument(UINT8 write,
+                                 UINT8 function,
+                                 UINT8 raw,
+                                 UINT32 address,
+                                 UINT8 data);
+UINT32 er_pi_sdio_cmd53_argument(UINT8 write,
+                                 UINT8 function,
+                                 UINT8 block_mode,
+                                 UINT8 incrementing_address,
+                                 UINT32 address,
+                                 UINT32 count);
+UINT32 er_pi_mmc_relative_card_argument(UINT32 relative_card_address);
+UINT8 er_pi_mmc_command_prepare(UINT32 command_index,
+                                UINT32 argument,
+                                UINT32 response_kind,
+                                ErPiMmcCommand* out_command);
+UINT8 er_pi_zero2w_sdio_identity_plan(ErPiZero2wSdioBringupPlan* out_plan);
+UINT8 er_pi_zero2w_sdio_claim_plan(UINT32 relative_card_address,
+                                   ErPiZero2wSdioBringupPlan* out_plan);
 UINT8 er_pi_zero2w_apply_boot_report(ErBootServicesReport* report);
 
 #endif
