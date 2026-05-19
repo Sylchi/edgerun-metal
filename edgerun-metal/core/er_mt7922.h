c5cf7790f70687d94a45e58dc0b39153779d1f4e
ken <ken@fw>
2026-05-20 02:15:38 +0700

    Load MT7922 firmware through boot device

--- edgerun-metal/core/er_mt7922.h
+++ edgerun-metal/core/er_mt7922.h
@@ -6,6 +6,7 @@
  * Intention: let BLE/Wi-Fi burst planning verify usable hardware without binding a full MAC yet.
  */
 
+#include "er_firmware_loader.h"
 #include "er_pci.h"
 #include "er_types.h"
 
@@ -19,9 +20,23 @@ typedef struct {
   UINT64 mmio_base;
 } ErMt7922PciDevice;
 
+typedef struct {
+  ErMt7922PciDevice pci;
+  ErFirmwareImage firmware;
+} ErMt7922BootDevice;
+
 UINT8 er_mt7922_pci_id_supported(UINT32 id);
 void er_mt7922_clear_pci_device(ErMt7922PciDevice* device);
 UINT8 er_mt7922_prepare_pci_device(const ErPciDeviceSnapshot* snapshot,
                                    ErMt7922PciDevice* out_device);
+void er_mt7922_clear_boot_device(ErMt7922BootDevice* device);
+UINT8 er_mt7922_prepare_boot_device(const ErCryptoProvider* crypto,
+                                    const ErBootConfig* config,
+                                    const ErPciDeviceSnapshot* snapshot,
+                                    ErFirmwareReadFn read_fn,
+                                    void* read_ctx,
+                                    UINT8* firmware_bytes,
+                                    UINTN firmware_capacity,
+                                    ErMt7922BootDevice* out_device);
 
 #endif
