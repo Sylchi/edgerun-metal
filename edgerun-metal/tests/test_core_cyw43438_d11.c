56600378e1ef941e87f1db06dd63f9840aa6292a
ken <ken@fw>
2026-05-20 14:02:38 +0700

    Stage CYW43438 D11 TX frame

--- edgerun-metal/tests/test_core_cyw43438_d11.c
+++ edgerun-metal/tests/test_core_cyw43438_d11.c
@@ -7,9 +7,18 @@ static void test_cyw43438_d11_registers(void) {
   check_uint64("cyw43438 d11 xmtfifocmd offset",
                ER_CYW43438_D11_XMTFIFOCMD,
                0x540u);
+  check_uint64("cyw43438 d11 template write pointer offset",
+               ER_CYW43438_D11_TPLATEWRPTR,
+               0x130u);
+  check_uint64("cyw43438 d11 template write data offset",
+               ER_CYW43438_D11_TPLATEWRDATA,
+               ER_CYW43438_D11_TPLATEWRPTR + (UINT32)sizeof(UINT32));
   check_uint64("cyw43438 d11 xmtfifordy offset",
                ER_CYW43438_D11_XMTFIFORDY,
                ER_CYW43438_D11_XMTFIFOCMD + 6u);
+  check_uint64("cyw43438 d11 fifo64 stride",
+               ER_CYW43438_D11_FIFO64_STRIDE,
+               0x40u);
   check_uint64("cyw43438 d11 bcmc fifo",
                ER_CYW43438_D11_TX_BCMC_FIFO,
                4u);
@@ -30,4 +39,14 @@ static void test_cyw43438_d11_registers(void) {
                ER_CYW43438_D11_TX_BCMC_FIFO_SELECT &
                    ER_CYW43438_D11_TXFIFOCMD_FIFOSEL_MASK,
                ER_CYW43438_D11_TX_BCMC_FIFO_SELECT);
+  check_uint64("cyw43438 d11 bcmc pio tx control",
+               ER_CYW43438_D11_TX_BCMC_PIO_TX_CONTROL,
+               ER_CYW43438_D11_FIFO64_BASE +
+                   (ER_CYW43438_D11_TX_BCMC_FIFO *
+                    ER_CYW43438_D11_FIFO64_STRIDE) +
+                   ER_CYW43438_D11_FIFO64_PIO_TX_OFFSET);
+  check_uint64("cyw43438 d11 bcmc pio tx data",
+               ER_CYW43438_D11_TX_BCMC_PIO_TX_DATA,
+               ER_CYW43438_D11_TX_BCMC_PIO_TX_CONTROL +
+                   (UINT32)sizeof(UINT32));
 }
