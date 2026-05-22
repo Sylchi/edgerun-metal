da00fc75fbc9533cc2ddc11dc7ac96b11ad23338
ken <ken@fw>
2026-05-22 08:38:47 +0700

    latest work

--- edgerun-metal/modules/c_hostcall_region_len/c_hostcall_region_len.c
+++
@@ -1,4 +0,0 @@
-extern i64 ui_emit(i64, i64) __import("edgerun.ui", "emit");
-extern i64 region_len(i64) __import("edgerun.memory", "region_len");
-memory(1);
-export i64 main(void) { return region_len(2); }
