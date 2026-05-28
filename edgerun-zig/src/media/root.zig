e5161e5c4b26215b14448f9d3fa2c6c280d1b5b3
Ken Kauksi <kensservices@gmail.com>
2026-05-28 12:48:08 +0700

    Expose only ERIMG runtime image contract

--- edgerun-zig/src/media/root.zig
+++ edgerun-zig/src/media/root.zig
@@ -10,41 +10,37 @@ pub const RuntimeImageError = image.RuntimeImageError;
 pub const RuntimeImageHeader = image.RuntimeImageHeader;
 pub const RuntimeImageView = image.RuntimeImageView;
 pub const Format = image.Format;
+pub const ImportFormat = image.ImportFormat;
+
+pub const detectFormat = image.detectFormat;
+pub const decodeHeader = image.decodeHeader;
+pub const decode = image.decode;
+pub const decodeRuntimeImage = image.decodeRuntimeImage;
+pub const decodeRuntimeRgba = image.decodeRuntimeRgba;
+
+pub const runtimeImageMagic = image.runtimeImageMagic;
+pub const runtimeImageHeaderSize = image.runtimeImageHeaderSize;
+pub const runtimeImageRgbaCanonicalLen = image.runtimeImageRgbaCanonicalLen;
+pub const runtimeImageRgbaCanonicalLenForTiling = image.runtimeImageRgbaCanonicalLenForTiling;
+pub const runtimeCanonicalLen = image.runtimeCanonicalLen;
+pub const runtimeCanonicalLenForHeader = image.runtimeCanonicalLenForHeader;
+pub const runtimeCanonicalLenForTiling = image.runtimeCanonicalLenForTiling;
+pub const runtimeCanonicalLenTiled = image.runtimeCanonicalLenTiled;
+pub const encodeRuntimeRgba = image.encodeRuntimeRgba;
+pub const encodeRuntimeRgbaTiled = image.encodeRuntimeRgbaTiled;
+pub const decodeRuntimeRgbaObject = image.decodeRuntimeRgbaObject;
+
+pub const importDetectFormat = image.importDetectFormat;
+pub const importHeader = image.importHeader;
+pub const importScratchByteLen = image.importScratchByteLen;
+pub const importToRuntimeWithScratch = image.importToRuntimeWithScratch;
+pub const importToRuntimeTiledWithScratch = image.importToRuntimeTiledWithScratch;
+pub const importToRuntimeDefaultTiledWithScratch = image.importToRuntimeDefaultTiledWithScratch;
 
 pub const WebpAnimationHeader = image.WebpAnimationHeader;
 pub const WebpAnimationFrameInfo = image.WebpAnimationFrameInfo;
 pub const WebpAnimationFrame = image.WebpAnimationFrame;
 pub const WebpAnimationCanvasDecoder = image.WebpAnimationCanvasDecoder;
-
-pub const detectFormat = image.detectFormat;
-pub const decodeHeader = image.decodeHeader;
-pub const decode = image.decode;
-pub const decodeWithScratch = image.decodeWithScratch;
-pub const scratchByteLen = image.scratchByteLen;
-
-pub const runtimeImageMagic = image.runtimeImageMagic;
-pub const runtimeImageHeaderSize = image.runtimeImageHeaderSize;
-pub const runtimeImageRgbaCanonicalLen = image.runtimeImageRgbaCanonicalLen;
-pub const runtimeCanonicalLen = image.runtimeCanonicalLen;
-pub const runtimeCanonicalLenForHeader = image.runtimeCanonicalLenForHeader;
-pub const encodeRuntimeRgba = image.encodeRuntimeRgba;
-pub const decodeRuntimeRgbaObject = image.decodeRuntimeRgbaObject;
-pub const decodeToRuntimeWithScratch = image.decodeToRuntimeWithScratch;
-pub const decodeRuntimeImage = image.decodeRuntimeImage;
-pub const decodeRuntimeRgba = image.decodeRuntimeRgba;
-
-pub const pngScratchByteLen = image.pngScratchByteLen;
-pub const decodePngHeader = image.decodePngHeader;
-pub const decodePng = image.decodePng;
-
-pub const decodeTgaHeader = image.decodeTgaHeader;
-pub const decodeTga = image.decodeTga;
-pub const encodeTgaRgba = image.encodeTgaRgba;
-
-pub const webpScratchByteLen = image.webpScratchByteLen;
-pub const decodeWebpHeader = image.decodeWebpHeader;
-pub const decodeWebp = image.decodeWebp;
-pub const decodeWebpWithScratch = image.decodeWebpWithScratch;
 pub const decodeWebpAnimationHeader = image.decodeWebpAnimationHeader;
 pub const decodeWebpAnimationFrame = image.decodeWebpAnimationFrame;
 pub const decodeWebpAnimationFrameWithScratch = image.decodeWebpAnimationFrameWithScratch;
