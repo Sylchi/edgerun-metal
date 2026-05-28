d13ef9f7b900021d6f8481423d144c87b7ebf08e
Ken Kauksi <kensservices@gmail.com>
2026-05-28 09:38:55 +0700

    Replace font atlas namespace re-export with explicit exports

--- edgerun-zig/src/render/font_atlas.zig
+++ edgerun-zig/src/render/font_atlas.zig
@@ -1,1 +1,9 @@
-pub usingnamespace @import("font_atlas_weighted.zig");
+const weighted = @import("font_atlas_weighted.zig");
+
+pub const width = weighted.width;
+pub const height = weighted.height;
+pub const bytes = weighted.bytes;
+pub const channels = weighted.channels;
+pub const glyph_capacity = weighted.glyph_capacity;
+pub const format = weighted.format;
+pub const Atlas = weighted.Atlas;
